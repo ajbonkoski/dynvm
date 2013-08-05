8463fb417e0bd2548faa0274085813fc30b43827
Anthony J Bonkoski <ajbonkoski@gmail.com>
2013-08-05 07:59:25 -0400

    improved the program suite runner

--- src/compare/run.d
+++ src/compare/run.d
@@ -8,10 +8,20 @@ import std.string;
 import std.process;
 import std.conv;
 import std.datetime;
+import std.c.stdlib;
 
 enum EXEC_MASK = 1<<6;
 enum SEPARATOR_SIZE = 55;
 enum LINE_SIZE = SEPARATOR_SIZE-20;
+enum REQUIRED_FILES = ["argv", "ans", "Makefile"];
+enum LANGUAGE_MAP = [
+  "":          "C Language",
+  "java.run":  "Java",
+  "py":        "CPython",
+  "js":        "Javascript V8"
+];
+
+string extToLang(string s) { return LANGUAGE_MAP[s]; }
 
 // a nice python-style 'c'*5 string utility
 auto s(char c, ulong times) { char[] s; s.length = times; foreach(i; 0..times) s[i] = c; return s;}
@@ -24,6 +34,25 @@ void write_output_line(A, B)(A a_, B b_)
   writef("  %s%s%s\n", a, s(' ', LINE_SIZE-a.length), b);
 }
 
+bool GOOD = true;
+void require_dir(string dir, void delegate() f){ require_file(dir, "", f); }
+void require_file(string dir, string file, void delegate() f)
+{ if(!buildPath(dir, file).exists) { GOOD = false; f(); } }
+
+void verify_dir(string dir)
+{
+  require_dir(dir, {
+      stderr.writef("Error: program suite '%s' doesn't exist\n", dir);
+      exit(1);
+  });
+
+  foreach(file; REQUIRED_FILES) require_file(dir, file, {
+      stderr.write("Error: expected to find '%s' for program suite '%s'\n", file, dir);
+  });
+
+  if(!GOOD) exit(1);
+}
+
 void build_dir(string dir)
 {
   string cmd = format("make -C %s", dir);
@@ -31,43 +60,56 @@ void build_dir(string dir)
   assert(res.status == 0, "Build failed");
 }
 
-void execute(string cmd)
+void execute(string name, string argv)
 {
   StopWatch sw;
+  auto cmd = format("%s %s", name, argv);
+  auto lang = name.baseName.split(".")[1..$].join(".").extToLang;
 
   sw.start();
   auto res = executeShell(cmd);
   sw.stop();
 
-  if(res.status == 0)
-    write_output_line(cmd, sw.peek().to!("seconds", double));
-  else
-    write_output_line(cmd, "FAILED");
+  if(res.status == 0) {
+    double dt = sw.peek().to!("seconds", double);
+    write_output_line(lang, format("%.3f", dt));
+  } else {
+    write_output_line(lang, "FAILED");
+  }
 }
 
-void main(string[] args)
+int main(string[] args)
 {
-  enforce(args.length >= 3);
-  auto dir = buildNormalizedPath(args[0].dirName, args[1]);
-  auto subargs = join(args[2..$]);
-  writeln(dir);
+  scope(exit) doCleanup(args);
+
+  if(args.length != 2) {
+    stderr.writef("usage: %s <name>\n", args[0]);
+    return 1;
+  }
+
+  auto dir = buildPath(args[0].dirName, args[1]);
+  dir.verify_dir();
+  auto subargs = buildPath(dir, "argv").readText.chomp;
 
   writef("Building %s...\n", dir);
   build_dir(dir);
 
   write_separator('=');
-  write_output_line("Language", "Time");
+  write_output_line("Language", "Time (in sec)");
   write_separator('=');
 
   foreach(dirent; dirEntries(dir, SpanMode.shallow)) {
     if(dirent.attributes & EXEC_MASK) {
-      execute(format("./%s %s", dirent.name, subargs));
+      execute(dirent.name, subargs);
     }
   }
 
+  return 0;
+}
 
-  // I Hate this being here, but rdmd doesn't cleanup properly, so here it is...
+// I Hate this being here, but rdmd doesn't cleanup properly, so here it is...
+void doCleanup(string[] args)
+{
   remove(args[0]);   // delete self, so its not accidently called
-  remove(buildNormalizedPath(args[0].dirName, "rdmd.deps"));
-
+  remove(buildPath(args[0].dirName, "rdmd.deps"));
 }

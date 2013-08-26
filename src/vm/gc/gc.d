bd74095a57db358365e27ba8ef2cef1928ed149c
Anthony J Bonkoski <ajbonkoski@gmail.com>
2013-08-26 18:59:24 -0400

    jit works in release mode now

--- src/vm/gc/gc.d
+++ src/vm/gc/gc.d
@@ -8,12 +8,47 @@ union GCObjectHeader
 
 void *GCAlloc(size_t size)
 {
-  return malloc(size);
+  return GCState.alloc(size);
 }
 
 T *GCAlloc(T)()
 {
-  void *mem = malloc(T.sizeof);
-  return cast(T*) mem;
+  return cast(T*) GCState.alloc(T.sizeof);
 }
 
+/************* Private State **************/
+private:
+
+struct GCStateStruct
+{
+  ubyte *mem_start = null;
+  ubyte *mem_end = null;
+  ubyte *mem_ptr = null;
+
+  static immutable BLOCK_SIZE = (2<<13);
+  void alloc_new_block()
+  {
+    mem_ptr = mem_start = cast(ubyte*) malloc(BLOCK_SIZE);
+    assert(mem_start !is null);
+    mem_end = mem_start += BLOCK_SIZE;
+  }
+
+  void *alloc(size_t size)
+  {
+    // assure the req size of 4-byte aligned
+    auto size_align = ((size-1)/4 + 1)*4;
+    void *ret = mem_ptr;
+    mem_ptr += size_align;
+    if(mem_ptr > mem_end) {
+      alloc_new_block();
+      ret = mem_ptr;
+      mem_ptr += size_align;
+      assert(mem_ptr > mem_end);
+    }
+
+    return ret;
+  }
+}
+
+static GCStateStruct GCState = void;
+static this() { GCState.alloc_new_block; }
